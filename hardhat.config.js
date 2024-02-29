6b51527feb30f0eae48ac4f8f7b5ba15ba633eed
SStech099 <99163081+SStech099@users.noreply.github.com>
2024-02-29 11:50:58 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,17 +5,17 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
   networks: {
     mumbai: {
-      url: "https://polygon-mumbai.g.alchemy.com/v2/UFcYwVr5ykNS-KUwHKG29fNwtTw5lgcq",
+      url: "https://polygon-mumbai.g.alchemy.com/v2/alchemy_api_key",
       chainId: 80001,
       accounts: [
-        "44452881056ec9992bde8b4f1f50f66e3e98004c0db920b63a79d81ff54060f2",
+        "private_key",
       ],
       gasPrice: 5000000000,
       gas: 50000000,
     },
   },
   etherscan: {
-    apiKey: "AXV5C1PRE16INB9G6QQM8RSCHAT886CVVN",
+    apiKey: "api_key",
   },
   sourcify: {
     enabled: true,
